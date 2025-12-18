a30f72f68d2bd30c4071dee42e124a1676979d23
Rashi <rashi.sanger09@gmail.com>
2025-12-18 11:53:21 +0530

    changes made in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import ReduxUi from './ReduxExample/ReduxUi'
 import TodoUI from './ToDoList/TodoUI'
 function App() {
   const [count, setCount] = useState(0)
 
   return(
     <div>
-      {/* <ReduxUi/> */}
       <TodoUI/>
     </div>
   )
